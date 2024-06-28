d3964ce007aa2c6f5497d5457c18a03868792aa2
Baronous <thuongnguyendinh959@gmail.com>
2024-06-29 05:08:04 +0700

    Done function!

--- app/src/main/java/com/example/hit_product/data/source/Account.kt
+++ app/src/main/java/com/example/hit_product/data/source/Account.kt
@@ -3,11 +3,10 @@ package com.example.hit_product.data.source
 data class Account(
     val userID: String,
     val username : String,
+    val password: String,
     val dateOfBirth : String,
     val dateOfClub1 : String,
     val dateOfClub2 : String
 ){
-    companion object{
 
-    }
 }
