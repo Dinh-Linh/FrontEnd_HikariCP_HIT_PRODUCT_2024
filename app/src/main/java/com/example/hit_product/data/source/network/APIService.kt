f3896ae8e581ccb8db401dbccfa810423b5230c7
Dinh-Linh <140419823+Dinh-Linh@users.noreply.github.com>
2024-07-06 23:34:23 +0700

    home

--- app/src/main/java/com/example/hit_product/data/source/network/APIService.kt
+++ app/src/main/java/com/example/hit_product/data/source/network/APIService.kt
@@ -1,4 +1,13 @@
 package com.example.hit_product.data.source.network
 
-interface APIService {
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+data class LoginRequest(val username: String, val password: String)
+data class LoginResponse(val token: String, val userId: Int)
+
+interface ApiService {
+    @POST("login")
+    fun login(@Body loginRequest: LoginRequest): Call<LoginResponse>
 }
